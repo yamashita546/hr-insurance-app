061a2d4c6ec2abb556fd23b155b716dbde09a66a
山下 裕樹 <yuki.yamashita@pathoslogos.co.jp>
2025-05-19 18:21:53 +0900

    firebase設定　ホスティングだけ完了

---
+++ src/environment/environment.ts
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD5fMsoF1Ifdp1H0jz5GfC5el1KmA_6-i8",
+    authDomain: "kensyu10089.firebaseapp.com",
+    projectId: "kensyu10089",
+    storageBucket: "kensyu10089.firebasestorage.app",
+    messagingSenderId: "151285599750",
+    appId: "1:151285599750:web:38ec15bd42f179a4b3bd87",
+    measurementId: "G-HJ8NBLVTZX"
+  };

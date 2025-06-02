a282f7afdb8a2003199f8aa7be95df64097b1006
山下 裕樹 <yuki.yamashita@pathoslogos.co.jp>
2025-06-02 15:23:06 +0900

    ユーザー情報表示追加　15時20分

--- src/app/features/auth/pages/manage-user/manage-user.component.ts
+++ src/app/features/auth/pages/manage-user/manage-user.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UserCompanyService } from '../../../../core/services/user-company.service';
+import { filter, take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-manage-user',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './manage-user.component.html',
   styleUrl: './manage-user.component.css'
 })
-export class ManageUserComponent {
+export class ManageUserComponent implements OnInit {
+  companyId: string = '';
+  companyName: string = '';
+  userName: string = '';
+  email: string = '';
+  role: string = '';
 
+  constructor(private userCompanyService: UserCompanyService) {}
+
+  ngOnInit(): void {
+    this.userCompanyService.company$
+      .pipe(filter(company => !!company && !!company.companyId), take(1))
+      .subscribe(company => {
+        this.companyId = company!.companyId;
+        this.companyName = company!.name;
+      });
+    this.userCompanyService.user$
+      .pipe(filter(user => !!user && !!user.uid), take(1))
+      .subscribe(user => {
+        this.userName = user!.displayName || '';
+        this.email = user!.email || '';
+        this.role = user!.role || '';
+      });
+  }
 }
